931807ef194888e371ab35f35269601f55b8fa41
Kornelia <kory0001@stud.kea.dk>
2024-09-03 11:48:26 +0200

    Singleview dynamisk

--- produkt.js
+++ produkt.js
@@ -11,47 +11,29 @@ function getProduct() {
 function visProdukt(product) {
   document.querySelector(".title").textContent = product.productdisplayname;
   document.querySelector("p").textContent = product.brandname;
-  document.querySelector(".p_description").textContent = product.season;
+  document.querySelector(".product_id").textContent = "Article ID: " + product.id;
+  document.querySelector(".product_category").textContent = "Category: " + product.category;
+  document.querySelector(".product_subcategory").textContent = "Subategory: " + product.subcategory;
+  document.querySelector(".product_articletype").textContent = "Articletype: " + product.articletype;
+  document.querySelector(".product_usagetype").textContent = "Usagetype: " + product.usagetype;
+  document.querySelector(".product_season").textContent = "Season: " + product.season;
+  document.querySelector(".product_productionyear").textContent = "Productionyear: " + product.productionyear;
+  document.querySelector(".pris_sigleview").textContent = product.price + ",-";
   document.querySelector("main img").src = `https://kea-alt-del.dk/t7/images/webp/640/${id}.webp`;
 }
 
 getProduct();
 
-//     <div><img src="img/bibs.webp" alt="bib"></div>
-//     <div>
-//         <h1 class="title">MENS PRO BIBS CYCLING SHORT</h1>
-//         <p class="p_intro">Our most comfortable and durable model yet.</p>
-//         <p>This bibs is an essential pair in any riders wardrobe. It is created to suit training efforts and long
-//             days in the saddle.
-//             This is also why it was made with the four-layer ultralight foam chamois, securing maximum comfort and
-//             durability, that moves with your body as you go.
-//             The micro-perforated straps are both comfortable and practical, as they keep the bibs in place at all
-//             times. The fabric is stretchy, and of highest quality available.
-//         </p>
-//         <br>
-//         <h4>Product details</h4>
-//         <ul>
-//             <li>
-//                 Four-layer ultralight foam chamois</li>
-//             <li>
-//                 Micro-perforated straps
-//             </li>
-//             <li>
-//                 Resilient to elastic degradation</li>
-//             <li>
-//                 UPF 40 low-profile shoulder straps
-//             </li>
-//             <li>
-//                 Fully-dyed fabrics</li>
-//             <li>
-//                 Fabric: 82% Polyester 18% Elastane</li>
-//             <li>
-//                 Made in Italy</li>
-//         </ul>
-//         <br>
-//         <p>1250,-</p>
-
-//         <div class="button_container">
-//             <a href="#" class="buy_now">Buy Now</a>
-//         </div>
-//     </div>
+// id	1163
+// gender	"Men"
+// category	"Apparel"
+// subcategory	"Topwear"
+// articletype	"Tshirts"
+// season	"Summer"
+// productionyear	2011
+// usagetype	"Sports"
+// productdisplayname	"Sahara Team India Fanwear Round Neck Jersey"
+// price	895
+// discount	null
+// brandname	"Nike"
+// soldout	0
